ffd5c43f79cd01d88824739bb91e966e25e8446b
fabio_magrotti <fabio.magrotti@gmail.com>
2021-01-29 15:12:07 +0100

    add paycheck controller class to app library for future improvement

--- lib/appLib.py
+++ lib/appLib.py
@@ -265,6 +265,387 @@ def check_paycheck_badges():
 
     return check
 
+class PaycheckController():
+    def __init__(self, paychecks_to_check, badges_to_check):
+        """
+        paychecks_to_check (str) -> path to multiple pages pdf containing all paychecks to check
+        """
+
+        self.badges_path = badges_to_check # path to CARTELLINI folder
+        self.paychecks_to_check = paychecks_to_check # lul_controllo
+        self.verify_filename = "Verifica.xlsx" #name of the output verification xlsx
+        self.default_configuration = {
+                "col_codes": {},
+                "col_to_extract": [
+                    "Z01100",
+                    "Z00246",
+                    "Z00255",
+                    "Z01138",
+                    "000279",
+                    "001008",
+                    "ZP0160",
+                    "ZP0162",
+                    "ZPS000",
+                    "F02701",
+                    "000282",
+                    "003955",
+                    "Z05031",
+                    "ZP0001",
+                    "ZP0030",
+                    "003951",
+                    "002099",
+                    "002101",
+                    "PLACEHOLDER1",
+                    "PLACEHOLDER2",
+                    "F09080",
+                    "F09100",
+                    "F09130",
+                    "F09081",
+                    "Z50022",
+                    "Z50023",
+                    "Z51000",
+                    "Z51010",
+                    "000085",
+                    "000086",
+                    "ZP8134",
+                    "ZP8138",
+                    "ZP8130",
+                    "003450",
+                    "000283",
+                    "quota t.f.r.",
+                    "quota t.f.r. a fondi"
+                ],
+                "merging_columns": {
+                    "Ferie/fest. pagate": [
+                        "Z01100",
+                        "Z01138",
+                        "Z00255",
+                        "Z51010",
+                        "Z00246",
+                        "Z51000"
+                    ],
+                    "T.F.R.": [
+                        "quota t.f.r.",
+                        "quota t.f.r. a fondi",
+                        "ZP8134",
+                        "003450",
+                        "fondo t.f.r. al 31/12"
+                    ],
+                    "Cessione 1/5": [
+                        "002101",
+                        "003951",
+                        "002099"
+                    ],
+                    "Assegni Familiari": [
+                        "ZP0160",
+                        "ZP0162"
+                    ]
+                }
+            }
+        self.config = self.__load_config()
+
+
+    """ PRIVATE METHODS """
+    def __load_config(self):
+        if os.path.exists("config_files/conversion_table.json"):
+            with open("config_files/conversion_table.json", "r") as f:
+                return json.load(f)
+        else:
+            return copy.deepcopy(self.default_configuration)
+
+
+    """ PUBLIC METHODS """
+    def create_config_from_csv(self, csv_path):
+        """
+        csv must have at least two columns 'Codice voce' and 'Descrizione voce'
+        """
+
+        with open(csv_path, 'rt')as f:
+            df = pd.read_csv(f, sep=";")
+            columns = {}
+
+            for index, row_ in df.iterrows():
+                row = dict(row_)
+
+                # parse out nan values
+                parsed_row = {}
+                for val in row:
+                    if not pd.isnull(row[val]):
+                        parsed_row[val] = row[val]
+
+                if parsed_row['Codice voce'] not in self.config:
+                    columns[parsed_row['Codice voce']] = parsed_row
+                    columns[parsed_row['Codice voce']]['col_value'] = -1
+
+        # adding keys
+        columns["quota t.f.r."] = {"Descrizione voce": "Quota T.F.R.", "col_value": -1}
+        columns["quota t.f.r. a fondi"] = {"Descrizione voce": "Quota T.F.R. a Fondi", "col_value": -1}
+
+        # setting self.config with parsed data
+        self.config["col_codes"] = columns
+
+        with open("config_files/conversion_table.json", "w") as f:
+            f.write(json.dumps(self.config, indent=4, ensure_ascii=True))
+
+        print(f"* * conversion_table.json created from this file >> {csv_path}")
+
+    def paycheck_verification(self, create_Excel=False):
+        PDF_file = self.paychecks_to_check
+        total_content = {}
+
+        col_codes = self.config['col_codes']
+        col_to_extract = self.config['col_to_extract']
+        merging_columns = self.config['merging_columns']
+
+        # if there are no columns to extract set in the configuration, raise exception
+        if not col_codes:
+            raise Exception("No columns specified for extraction in config file")
+
+        inputpdf = fitz.open(PDF_file)
+
+        # per ogni pagina
+        for i in range(inputpdf.pageCount):
+            page = inputpdf.loadPage(i)
+            blocks = page.getText("blocks")
+            blocks.sort(key=lambda block: block[1])  # sort vertically ascending
+
+            ####### find name in page
+            name = None
+            for index, b in enumerate(blocks):
+                for elem in b:
+                    elem = str(elem)
+                    if ('cognome' in elem and 'nome' in elem) or ('COGNOME' in elem and 'NOME' in elem):
+                        name_array = (blocks[index+1][4]).split('\n')
+
+                        # conditional fix array
+                        if len(name_array) == 2:
+                            for check_ in name_array:
+                                if "cessat" in check_.lower():
+                                    name_array = (blocks[index+2][4]).split('\n')
+                                    break
+
+                        #check if there's a name in the array wich was found in the block
+                        for value in name_array:
+                            valid = True
+                            for char in value:
+                                if char.isdigit():
+                                    valid = False
+                                    break
+
+                            if valid and value:
+                                if value != None and not value.isspace() and value[0].isalpha():
+                                    name = value
+                                    break
+
+            if name:
+                name = name.upper()
+
+                # add name to total_content
+                if name not in total_content:
+                    total_content[name] = {}
+
+                ####### get page content
+                for index, b in enumerate(blocks):
+                    content = b[4].split('\n')
+                    content = [string for string in content if (string != "" and not string.startswith("   "))]
+
+                    # parsing content
+                    for i, elem in enumerate(content):
+
+                        # get netto
+                        if "NETTOsDELsMESE" in elem:
+                            netto = blocks[index+1][-3]
+                            netto = netto.replace("€", "").strip().replace(",",".")
+                            netto = netto.replace(".", "", (netto.count('.') - 1))
+                            try:
+                                netto = float(netto)
+                                total_content[name]["Netto del Mese"] = netto
+                            except:
+                                pass
+
+                        elem = (elem.replace("*", "")).strip()
+
+                        for paycheck_code in col_codes:
+
+                            if (paycheck_code.lower() == elem.split(" ")[0].lower()) \
+                                    or (len(paycheck_code.split(" ")) > 1) and elem.lower().startswith(paycheck_code.lower()):
+
+                                # check if value is to be extracted
+                                if paycheck_code in col_to_extract:
+
+                                    # check if it needs to be merged
+                                    elem_colname = None
+                                    for colname in merging_columns:
+                                        for subname in merging_columns[colname]:
+                                            if subname == paycheck_code:
+                                                elem_colname = colname
+
+                                    if not elem_colname:
+                                        elem_colname =  col_codes[paycheck_code]['Descrizione voce']
+
+                                    # get row value
+                                    val = content[col_codes[paycheck_code]['col_value']].replace(",", ".")
+                                    val = val.replace(".", "", (val.count('.') - 1))
+                                    try:
+                                        val = float(val)
+                                    except:
+                                        pass
+
+                                    # report data in the proper column
+                                    if elem_colname not in total_content[name]:
+                                        total_content[name][elem_colname] = val
+                                    else:
+                                        total_content[name][elem_colname] += val
+
+        if create_Excel:
+            sheet_name = "Verifica Buste Paga"
+            self.create_Excel(total_content, sheet_name)
+            print(f"File {self.verify_filename} generato con successo, {sheet_name} aggiunto al suo interno")
+
+    def badges_verification(self, create_Excel=False):
+
+        def parse_decimal_time(decimal_time):
+
+            #if 0 not present attach it
+            if len(decimal_time) == 1:
+                decimal_time = decimal_time + "0"
+
+            hour_min = 60
+            result = (100*int(decimal_time))/hour_min
+            return str(int(result))
+
+        total_content = {}
+
+        #for each badge
+        for file in os.listdir(self.badges_path):
+            path_to_badge = "/".join((self.badges_path, file))
+            full_name = None
+            last_block = []
+
+            inputpdf = fitz.open(path_to_badge)
+
+            ####### find name
+            page = inputpdf.loadPage(0)
+            blocks = page.getText("blocks")
+            blocks.sort(key=lambda block: block[1])  # sort vertically ascending
+
+            for index, b in enumerate(blocks):
+                if "cognome e nome" in b[4].split('\n')[0].lower():
+                    full_name = blocks[index+1][-3].split('\n')[0]
+                    full_name = " ".join(full_name.split())
+                    break
+
+            ###### find values
+            if full_name:
+                total_content[full_name] = {"Ore ordinarie": 0.0, "Ore straordinarie": 0.0}
+
+                for index, b in enumerate(blocks):
+
+                    ##### check if block is a day
+                    if len(b[4].split()[0][:-1]) <= 2 and b[4].split()[0][-1].isalpha() and b[4].split()[0][:-1].isdigit():
+                        day = b[4].split()
+                        day, day_values_ = day[0], day[1:]
+
+                        # check if data is valid
+                        if len(day[1:])%2 != 0 and day[1:][0].isdigit():
+                            raise Exception(f"Expected even pairs, got uneven pairs for worker {full_name} on day {day}")
+
+                        day_values_ = list(zip(day_values_[0::2], day_values_[1::2]))
+                        day_values = []
+
+                        # parse tuples in day values_
+                        for i_, tupla in enumerate(day_values_):
+                            # remove errors and start/end of workshift
+                            if len((tupla[0])) > 4 or (len(tupla[0]) == 4 and "," not in tupla[0]):
+                                continue
+                            if len(day_values) < 2:
+                                day_values.append(tupla)
+
+                        # if not hours as first value skip day
+                        if day_values:
+                            if len(day_values[0][0]) == 3:
+                                pass
+                            else:
+                                # parse decimal time values to number values
+                                value_to_add = day_values[0][0].replace(",", ".").split(".")
+                                if len(value_to_add) == 2:
+                                    if value_to_add[1] != "00" and value_to_add[1] != "0":
+                                        value_to_add[1] = parse_decimal_time(value_to_add[1])
+                                    value_to_add = float(".".join(value_to_add))
+                                else:
+                                    value_to_add = float(day_values[0][0])
+                                # if there's more than a pair of values in the day
+                                if len(day_values) > 1:
+                                    # if hours are overtime skip them
+                                    if day_values[1][0] == "306" or day_values[1][0] == "006":
+                                        pass
+                                    else:
+                                        total_content[full_name]['Ore ordinarie'] += value_to_add
+                                # otherwise assume they are ordinary hours
+                                else:
+                                    total_content[full_name]['Ore ordinarie'] += value_to_add
+
+                    ##### check if block is last and parse its data
+                    if b[4].split()[0].isdigit() and not b[4].split()[0][-1].isalpha() and len(b[4].split()[0])<=3:
+                        last_block_values = b[4].split()
+
+                        # parse values
+                        for val in last_block_values:
+                            is_number = True
+                            for letter in val:
+                                if letter.isalpha() and letter != ",":
+                                    is_number = False
+                            # append conditionally
+                            if is_number:
+                                if "," in val:
+                                    last_block.append(float(val.replace(",",".")))
+                                else:
+                                    last_block.append(int(val))
+                            else:
+                                if type(last_block[-1]) == int or type(last_block[-1]) == float:
+                                    last_block.append(val)
+                                else:
+                                    last_block[-1] = last_block[-1] + " " + val
+
+                        # check if data is valid
+                        if len(last_block)%3 != 0:
+                            raise Exception(f"Expected triplets on badge footer values for worker {full_name}")
+
+                ##### last fixes
+                total_content[full_name]["Ore ordinarie"] = total_content[full_name]["Ore ordinarie"]
+                last_block = list(zip(last_block[0::3], last_block[1::3], last_block[2::3]))
+                for pair in last_block:
+
+                    #parse pair[2] time value to number value
+                    pair_value = str(pair[2]).replace(",", ".").split(".")
+                    if len(pair_value) == 2:
+                        if pair_value[1] != "00" and pair_value[1] != "0":
+                            pair_value[1] = parse_decimal_time(pair_value[1])
+                        pair_value = float(".".join(pair_value))
+
+                    if pair[0] == 306 or pair[0] == 6:
+                        total_content[full_name]["Ore straordinarie"] += pair_value
+                    total_content[full_name][str(pair[0]) + " " + str(pair[1])] = pair_value
+
+        if create_Excel:
+            sheet_name = "Verifica Cartellini"
+            self.create_Excel(total_content, sheet_name)
+            print(f"File {self.verify_filename} generato con successo, {sheet_name} aggiunto al suo interno")
+
+    def create_Excel(self, content, sheet_name):
+        df = pd.DataFrame.from_dict(content).T
+
+        # sort alphabetically
+        df = df.sort_index()
+
+        open_mode = "a" if os.path.exists(self.verify_filename) else "w"
+
+        with pd.ExcelWriter(self.verify_filename, mode=open_mode) as writer:
+            df.to_excel(writer, sheet_name=sheet_name)
+
+    def get_drive_file(self, filename):
+        pass
+
 
 ''' MAILS '''
 
